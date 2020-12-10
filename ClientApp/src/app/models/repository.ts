85701629a9fe45b5e51d1db4f98f98ae0363d7b5
Laurie Roy <laurieroy.dev@gmail.com>
2020-12-09 17:23:38 -0700

    update bootstrap; add a loading

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -2,15 +2,24 @@ import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Movie } from './movie.model';
 
+const moviesUrl = '/api/movies';
 @Injectable()
 export class Repository {
+
   public movie: Movie;
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Movie>(baseUrl + 'api/SampleData/GetMovie').subscribe(
-      (result) => {
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Movie>(baseUrl + 'api/movies/GetMovie')
+      .subscribe((result) => {
         this.movie = result;
       },
       (error) => console.error(error)
     );
+    this.getMovie(3);
+  }
+
+  getMovie(id: number) {
+    // console.log("Movie Data Requested");
+    return this.http.get(moviesUrl + '/' + id)
+      .subscribe(response => { this.movie = response; });
   }
 }
