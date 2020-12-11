02515f2db29fd28918324c8b97bb3036f38ef011
Laurie Roy <laurieroy.dev@gmail.com>
2020-12-10 22:02:53 -0700

    Add font-awesome; star ratings

--- ClientApp/src/app/store/ratings/ratings.component.ts
+++ ClientApp/src/app/store/ratings/ratings.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie.model';
 
 @Component({
   selector: 'app-ratings',
   templateUrl: './ratings.component.html',
-  styleUrls: ['./ratings.component.css']
+  styleUrls: ['./ratings.component.css'],
 })
-export class RatingsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class RatingsComponent {
+  @Input()
+  movie: Movie;
+  get stars(): boolean[] {
+    if (this.movie != null && this.movie.ratings != null) {
+      const total = this.movie.ratings
+        .map((r) => r.stars)
+        .reduce((prev, curr) => prev + curr, 0);
+      const count = Math.round(total / this.movie.ratings.length);
+      return Array(5)
+        .fill(false)
+        .map((value, index) => {
+          return index < count;
+        });
+    } else {
+      return [];
+    }
   }
-
 }
