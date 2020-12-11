4f0a52740bcaeb31be5dbe13ff1715dd3fd5ca02
Laurie Roy <laurieroy.dev@gmail.com>
2020-12-10 22:30:46 -0700

    Add cart

--- ClientApp/src/app/models/model.module.ts
+++ ClientApp/src/app/models/model.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Repository } from './repository';
+import { Cart } from './cart.model';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [],
-  providers: [Repository]
+  providers: [Repository, Cart]
 })
 export class ModelModule { }
